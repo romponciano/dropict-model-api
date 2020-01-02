1478f80dc69a896121f2ee4d12e49abe95f2b3cc
Rômulo Ponciano <romulo_37@hotmail.com>
2020-01-02 00:02:37 -0300

    set new pass to authenticate

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import os
+from hashlib import sha256
+# externals must be added to requirements.txt
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+passAuth = '7a61b53701befdae0eeeffaecc73f14e20b537bb0f8b91ad7c2936dc63562b25'
+
+def check_permissions():
+  hashedWord = sha256(request.headers.get('Authorization').encode('utf-8')).hexdigest()
+  print(hashedWord)
+  if hashedWord == passAuth:
+      return jsonify({"message": "a resposta para a vida, o universo e tudo mais"})
+  return jsonify({"message": "Não entre em pânico!"})
+
 @app.route('/')
-def nao_entre_em_panico():
-    if request.headers.get('Authorization') == '42':
-        return jsonify({"42": "a resposta para a vida, o universo e tudo mais"})
-    return jsonify({"message": "Não entre em pânico!"})
+def home():
+  return check_permissions()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
