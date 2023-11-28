8890b5c1291aab2f8ac68dee967a866f664742da
Romulo Ponciano <romponciano@gmail.com>
2023-11-28 14:13:44 -0300

    change app run to reploy on Render

--- main.py
+++ main.py
@@ -91,5 +91,4 @@ if __name__ == "__main__":
     dnn_model = joblib.load('./static/dnn.sav')
     knn_model = joblib.load('./static/knn.sav')
 
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
